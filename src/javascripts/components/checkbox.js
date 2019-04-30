3077163c3d67da1092440c97e03170663e466aa0
Mary West <westmarye1219@gmail.com>
2019-04-30 18:42:20 -0500

    fixing errors

--- src/javascripts/components/checkbox.js
+++ src/javascripts/components/checkbox.js
@@ -1,7 +1,6 @@
 const fontSize = document.getElementById('largeText');
 const darkBackground = document.getElementById('darkTheme');
 const messagesDiv = document.getElementById('messages');
-const card = document.getElementsByClassName('card');
 
 const largeText = () => {
   if (fontSize.checked === true) {
@@ -11,8 +10,20 @@ const largeText = () => {
   }
 };
 
+const cardBackground = () => {
+  const cardDark = document.getElementsByClassName('card');
+  for (let i = 0; i < cardDark.length; i+=1) {
+    if (darkBackground.checked === true) {
+      cardDark[i].style.backgroundColor = '#000000';
+      cardDark[i].style.color = '#FFFFFF';
+    } else {
+      cardDark[i].style.backgroundColor = '#FFFFFF';
+      cardDark[i].style.color = '#000000';
+    }
+  }
+};
+
 const darkTheme = () => {
-  console.error(darkBackground === true);
   if (darkBackground.checked === true) {
     messagesDiv.style.backgroundColor = '#000000';
     messagesDiv.style.color = '#FFFFFF';
@@ -23,22 +34,6 @@ const darkTheme = () => {
     cardBackground();
   }
 };
-
-
-const cardBackground = () => {
-  const cardDark = document.getElementsByClassName('card');
-  for (let i = 0; i < cardDark.length; i++) {
-    console.error(darkBackground.checked === true);
-    if (darkBackground.checked === true) {
-      cardDark[i].style.backgroundColor = '#000000';
-      cardDark[i].style.color = '#FFFFFF';
-    } else {
-      cardDark[i].style.backgroundColor = '#FFFFFF';
-      cardDark[i].style.color = '#000000';
-    }
-
-}
-}
 
 const checkBoxEvents = () => {
   fontSize.addEventListener('click', largeText);
