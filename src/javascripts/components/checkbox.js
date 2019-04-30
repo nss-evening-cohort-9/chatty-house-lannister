1ef2bbb76fdba75100516ce8f7f953f2d6623247
Mary West <westmarye1219@gmail.com>
2019-04-30 18:17:04 -0500

    darktheme and change of font work

--- src/javascripts/components/checkbox.js
+++ src/javascripts/components/checkbox.js
@@ -1,6 +1,7 @@
 const fontSize = document.getElementById('largeText');
 const darkBackground = document.getElementById('darkTheme');
 const messagesDiv = document.getElementById('messages');
+const card = document.getElementsByClassName('card');
 
 const largeText = () => {
   if (fontSize.checked === true) {
@@ -14,11 +15,29 @@ const darkTheme = () => {
   if (darkBackground.checked === true) {
     messagesDiv.style.backgroundColor = '#000000';
     messagesDiv.style.color = '#FFFFFF';
+    cardBackground();
   } else {
     messagesDiv.style.backgroundColor = '##FFFFFF';
     messagesDiv.style.color = '#000000';
+    cardBackground();
   }
 };
+
+
+const cardBackground = () => {
+  const cardDark = document.getElementsByClassName('card');
+  for (let i = 0; i < cardDark.length; i++) {
+    console.error(darkBackground.checked === true);
+    if (darkBackground.checked === true) {
+      cardDark[i].style.backgroundColor = '#000000';
+      cardDark[i].style.color = '#FFFFFF';
+    } else {
+      cardDark[i].style.backgroundColor = '#FFFFFF';
+      cardDark[i].style.color = '#000000';
+    }
+
+}
+}
 
 const checkBoxEvents = () => {
   fontSize.addEventListener('click', largeText);
