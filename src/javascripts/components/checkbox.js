c5ba71304746517bd10c539b97306a592ff130c2
Mary West <westmarye1219@gmail.com>
2019-04-29 22:24:19 -0500

    wip

--- src/javascripts/components/checkbox.js
+++ src/javascripts/components/checkbox.js
@@ -1,31 +1,28 @@
-let fontSize = document.getElementById('largeText')
-let darkBackground = document.getElementById('darkTheme')
-let messagesDiv = document.getElementById('messages');
-
+const fontSize = document.getElementById('largeText');
+const darkBackground = document.getElementById('darkTheme');
+const messagesDiv = document.getElementById('messages');
 
 const largeText = () => {
-    if (fontSize.checked === true) {
-      messagesDiv.style.fontSize = '20px';
-    }
-    else {
-      messagesDiv.style.fontSize = '12px';
-    }
-}
+  if (fontSize.checked === true) {
+    messagesDiv.style.fontSize = '20px';
+  } else {
+    messagesDiv.style.fontSize = '12px';
+  }
+};
 
 const darkTheme = () => {
   if (darkBackground.checked === true) {
     messagesDiv.style.backgroundColor = '#000000';
     messagesDiv.style.color = '#FFFFFF';
+  } else {
+    messagesDiv.style.backgroundColor = '##FFFFFF';
+    messagesDiv.style.color = '#000000';
   }
-  else {
-    messagesDiv.style.backgroundColor = '#000000';
-    messagesDiv.style.color = '#FFFFFF';
-  }
-}
+};
 
 const checkBoxEvents = () => {
   fontSize.addEventListener('click', largeText);
-  darkTheme.addEventListener('click', darkTheme);
-}
+  darkBackground.addEventListener('click', darkTheme);
+};
 
-export default { checkBoxEvents }
+export default { checkBoxEvents };
