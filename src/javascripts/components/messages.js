c3b02b52533fa0bdadbc7c12f5085dc52af2a21a
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-04-29 20:40:05 -0600

    some styling work

--- src/javascripts/components/messages.js
+++ src/javascripts/components/messages.js
@@ -6,10 +6,13 @@ let messages = [];
 const domStringBuilder = () => {
   let domString = '';
   messages.forEach((message) => {
-    domString += `<h2 id="username">${message.username}"</h2>`;
+    domString += '<div class="card">';
+    domString += `<h2 id="username">${message.username}</h2>`;
     domString += `<p id="messageText">${message.messageText}</p>`;
     domString += `<p id="id">${message.id} </p>`;
     domString += `<h6 id="timestamp">${message.timestamp} </h6>`;
+    domString += '<button type="button" class="btn btn-danger">Delete</button>';
+    domString += '</div>';
     domString += '</div>';
   });
   util.printToDom('messages', domString);
